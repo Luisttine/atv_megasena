0f7edb86e9a22426338cadfef86e857591c08ce7
Luís Souza <63884418+Luisttine@users.noreply.github.com>
2022-11-21 10:27:39 +0000

    styles dark and light

--- front/src/pages/Principal.tsx
+++ front/src/pages/Principal.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components";
 import { Acumulado } from "../components/Acumulado/index";
 import { Cabecalho } from "../components/Cabecalho";
 import { Local } from "../components/Local/index";
 import { NumeroSorteado } from "../components/NumerosSorte/index";
 import { ProxSorteio } from "../components/ProxSorteio";
 import services from "../services";
+import { light, dark } from "../styles/theme";
 import { Props } from "../types";
+import { WrapperSld } from "./styles";
 
 export default function Principal() {
     const [concurso, setConcurso] = useState({} as Props);
+    const [tema, setTema] = useState(light)
 
     useEffect(
         function(){
@@ -18,20 +22,22 @@ export default function Principal() {
                 const concurso: Props = await services.get(numero);
                 console.log(concurso)
                 setConcurso(concurso);
-            }
-        )()
+                setTema(parseInt(concurso.listaDezenas[0])%2 === 0 ? light:dark)
+            })()
         },
         []
     );
     return (
-        <>
-            <Cabecalho numero={concurso.numero} dataApuracao={concurso.dataApuracao}/>
-            {concurso.acumulado &&
-                <Acumulado />
-            }
-            <Local localSorteio={concurso.localSorteio} nomeMunicipioUFSorteio={concurso.nomeMunicipioUFSorteio} />
-            {concurso.listaDezenas && <NumeroSorteado numeroSorteio={concurso.listaDezenas}/>}
-            <ProxSorteio dataProximoConcurso={concurso.dataProximoConcurso} valorEstimadoProximoConcurso={concurso.valorEstimadoProximoConcurso}/>
-        </>
+        <ThemeProvider theme={tema}>
+            <WrapperSld>
+                <Cabecalho numero={concurso.numero} dataApuracao={concurso.dataApuracao}/>
+                {concurso.acumulado &&
+                    <Acumulado />
+                }
+                <Local localSorteio={concurso.localSorteio} nomeMunicipioUFSorteio={concurso.nomeMunicipioUFSorteio} />
+                {concurso.listaDezenas && <NumeroSorteado numeroSorteio={concurso.listaDezenas}/>}
+                <ProxSorteio dataProximoConcurso={concurso.dataProximoConcurso} valorEstimadoProximoConcurso={concurso.valorEstimadoProximoConcurso}/>
+            </WrapperSld>
+        </ThemeProvider>
     );
 }
---
+++ front/src/styles/theme.ts
@@ -0,0 +1,22 @@
+const dark = {
+    background: "#000",
+    text: "#d3d3d3",
+    strong: "#00cc00",
+    backgroundNumber: "#90ff90",
+    colorNumber: "#000",
+    colorNext: "#abe4ab"
+}
+
+const light = {
+    background: "#fff", 
+    text: "#6e6e6e",
+    strong: "#0066b3",
+    backgroundNumber: "#209869",
+    colorNumber: "#fff",
+    colorNext: "#4c546d"
+}
+
+export {
+    dark,
+    light
+}
